1a0fd013d2ff67d2f1743ff06db222e33510fce7
Bekbolot <nurmanbetovbekbolot@gmail.com>
2021-03-27 21:14:25 +0100

    fix bugs

--- src/shapes/Line.java
+++ src/shapes/Line.java
@@ -1,10 +1,10 @@
 package shapes;
 
-public class Line extends Polygon{
-   private final Point start;
-   private final Point end;
+public class Line extends Polygon {
+    private final Point start;
+    private final Point end;
 
-    public Line(Point start, Point end) throws NullPointerException{
+    public Line(Point start, Point end) throws NullPointerException {
         this.start = start;
         this.end = end;
     }
@@ -12,6 +12,13 @@ public class Line extends Polygon{
     @Override
     public void draw() {
         getWhiteBoard().removeShape(representation);
-        getWhiteBoard().drawLine(start.getX(), start.getY(), end.getX(), end.getY());
+        representation = getWhiteBoard().drawLine(start.getX(), start.getY(), end.getX(), end.getY());
+    }
+
+    @Override
+    public Polygon move(int dx, int dy) {
+        start.move(dx, dy);
+        end.move(dx, dy);
+        return this;
     }
 }
