86dfae6af544850bc3c8c1628e16d84b8b19cedd
Alybek Dandaev <dandaev.alybek@gmail.com>
2021-03-27 16:21:16 +0100

    Class Line updated(draw() implemented)

--- src/shapes/Line.java
+++ src/shapes/Line.java
@@ -1,5 +1,7 @@
 package shapes;
 
+import java.awt.*;
+
 public class Line extends Polygon{
    private Point point1;
    private Point point2;
@@ -8,4 +10,21 @@ public class Line extends Polygon{
         this.point1 = point1;
         this.point2 = point2;
     }
+
+    @Override
+    public void draw() {
+        getWhiteBoard().removeShape(representation);
+        getWhiteBoard().drawLine(point1.getX(),point1.getY(),point2.getX(),point2.getY());
+    }
+
+    /**
+     * Drawing Line with color
+     * @param  color of line
+     */
+    public void draw(Color color) {
+        getWhiteBoard().removeShape(representation);
+        getWhiteBoard().drawLine(point1.getX(),point1.getY(),point2.getX(),point2.getY(),color);
+    }
+
+
 }
